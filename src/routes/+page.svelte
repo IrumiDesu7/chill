d2544382f5d2984b551ca5d557ee08da5a904cfc
ilmi <ikalam89@gmail.com>
2025-06-10 21:05:55 +0800

    chore: init shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center gap-4">
+	<h1 class="text-4xl font-bold">Welcome to Chill</h1>
+	<p class="text-lg">The best way to chill</p>
+	<Button href="/login">Login</Button>
+	<Button href="/register">Register</Button>
+</div>
